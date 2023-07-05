66abd5b25953c6113264aad813918fd6ac7bc991
Henrique Santos <henrique.osantos10@gmail.com>
2023-07-05 13:30:21 -0300

    Avatar updated

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -31,11 +31,11 @@ export function Profile() {
    }
 
     async function handleUpdate(){
-        const user = { name, email, password: passwordNew, old_password: passwordOld }
+        const userUpdated = { name, email, password: passwordNew, old_password: passwordOld }
         
-        const userUpdated = Object.assign(user, updated);
-
-        await updateProfile({ user: userUpdated, avatarFile });
+        const userAssigned = Object.assign(user, userUpdated)
+   
+        await updateProfile({user: userAssigned, avatarFile});
     }
 
    
